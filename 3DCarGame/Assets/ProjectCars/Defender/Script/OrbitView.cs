0138e33b764ecf95c63f4eed0d1b282e3ac2b863
AtakanTurgut <77547962+AtakanTurgut@users.noreply.github.com>
2022-12-18 16:49:06 +0300

    -

---
+++ 3DCarGame/Assets/ProjectCars/Defender/Script/OrbitView.cs
@@ -0,0 +1,85 @@
+﻿/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitView : MonoBehaviour
+{
+    public WheelCollider sagOn, solOn, sagArka, solArka;
+
+    public float motorHizi, donmeHizi;
+
+    void Start()
+    {
+        
+    }
+ 
+    void Update()
+    {
+        sagArka.motorTorque = motorHizi * Input.GetAxis("Vertical");
+        solArka.motorTorque = motorHizi * Input.GetAxis("Vertical");
+
+        sagOn.steerAngle = donmeHizi * Input.GetAxis("Horizontal");
+        solOn.steerAngle = donmeHizi * Input.GetAxis("Horizontal");
+    }
+
+    public Collider target;
+    void OnTriggerEnter(Collider carTrigger)     
+    {
+        if (carTrigger == target)
+        {
+            print("Bump!");                 
+            motorHizi = 0;
+        }
+
+    }
+}
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitView : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    public float hiz = 0.0F;
+    public float donusHizi = 0.0F;
+    public float yukselme = 0.0F;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * hiz);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, Input.GetAxis("Vertical") * Time.deltaTime * hiz);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, donusHizi * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, -donusHizi * Time.deltaTime, 0);
+        }
+
+    }
+
+    public Collider target;  // Terrain ve Triggerden ötürü olmuyor 
+    void OnTriggerEnter(Collider carTrigger)
+    {
+        if (carTrigger == target)
+        {
+            print("Bump!");
+            hiz = 0;
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # 3DCarGame
 The game I once did as homework.
+Movement scripts in this directory = /Assets/ProjectCars/Defender/Script/OrbitView.cs
+
+1. [From the game]
+![](/pictures/3DCarGameSS.PNG)
+
+2. [Car image]
+![](/pictures/3DCarGameCar.PNG)
+
+3. [Map image]
+![](/pictures/3DCarGameMap.PNG)
